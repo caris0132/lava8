4869442c2b16acede7c06769acb79ef30c5d0959
caris0132 <nguyenductri1h1@gmail.com>
2022-03-20 18:26:31 +0700

    add force to https on producttion env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,9 +16,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        if (env('APP_ENV') == 'Production') {
-            URL::forceScheme('https');
-        }
     }
 
     /**
@@ -29,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (env('APP_ENV') == 'Production') {
+            URL::forceScheme('https');
+        }
     }
 }
